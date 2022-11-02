999d37cd2d1abdf7427e71376dff85c1f2a8c066
Akhtar <akhtarcdgl@gmail.com>
2022-11-02 16:04:44 +0530

    Prime Check File Created

---
+++ Prime_Check.js
@@ -0,0 +1,17 @@
+function checkPrime(question){
+
+let count=0;
+for  (let i = 1 ; i <= question ; i++ ){
+    if (question%2===0){
+        count++;
+    }
+}
+
+if (count===2){
+    console.log(question, "Is a Prime Number");
+}else {
+ console.log(question, " Is not a prime Number")   
+}
+}
+
+checkPrime(19);
